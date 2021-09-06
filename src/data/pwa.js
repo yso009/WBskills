f3605c2a6eee933ee5a70611b7480ebfc718189b
kimkiyung <yso009@gmail.com>
2021-09-06 20:33:07 +0900

    4학년 상세내용 수정(#57)

--- src/data/pwa.js
+++ src/data/pwa.js
@@ -7,131 +7,110 @@ export const pwaCollection = {
 				{
 					name: "인턴쉽I",
 					description: {
-						text: "Learn how to store data for your web app.",
+						text: "InternshipⅠ",
 						links: [
-							["MDN - Storage", "https://developer.mozilla.org/en-US/docs/Web/API/Storage_API"],
-							["MDN - Web Storage API", "https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API"],
-							["MDN - Cookies", "https://developer.mozilla.org/en-US/docs/Web/HTTP/Cookies"],
-							["MDN - IndexedDB API", "https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API"],
-							["Google Devs - View And Edit Local Storage", "https://developers.google.com/web/tools/chrome-devtools/storage/localstorage"],
-							["MDN - Client-side storage", "https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage"],
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111028", ""],
+							["학점 : 3-0-4주", ""],
 						]
 					}
 				},
 				{
 					name: "인턴쉽IV",
 					description: {
-						text: "Learn what routing means in single page applications.",
+						text: "InternshipⅣ",
 						links: [
-							["How Single-Page Applications Work", "https://blog.pshrmn.com/how-single-page-applications-work/"],
-							["Static Apps - Routing URLs in Static Web Apps", "https://staticapps.org/articles/routing-urls-in-static-apps/"],
-							["Routing With Single Page Applications or Client Side Routing", "https://pennyforyourcode.com/tldr-routing-with-single-page-applications-or-client-side-routing-327626cdc5dd"],
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111029", ""],
+							["학점 : 15-0-16주", ""],
 						]
 					}
 				},
 				{
 					name: "실무프로젝트I",
 					description: {
-						text: "Learn how to register a service worker to provide a rich native-like experience.",
+						text: "Practical Software ProjectⅠ",
 						links: [
-							["Google Devs - Service Workers: an Introduction", "https://developers.google.com/web/fundamentals/primers/service-workers"],
-							["Google Devs - The Service Worker Lifecycle", "https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle"],
-							["Google Devs - Service Worker Registration", "https://developers.google.com/web/fundamentals/primers/service-workers/registration"],
-							["MDN - Service Worker API", "https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers"],
-							["Google Devs - High-performance service worker loading", "https://developers.google.com/web/fundamentals/primers/service-workers/high-performance-loading"],
-							["Codelabs - Scripting the service worker", "https://codelabs.developers.google.com/codelabs/pwa-scripting-the-service-worker/index.html?index=..%2F..dev-pwa-training#0"],
-							["Codelabs - Caching files with the service worker", "https://codelabs.developers.google.com/codelabs/pwa-caching-service-worker/index.html?index=..%2F..dev-pwa-training#0"],
-							["Codelabs - Adding a Service Worker", "https://developers.google.com/web/fundamentals/codelabs/offline"],
-							["Google Devs - Debugging Service Workers", "https://developers.google.com/web/fundamentals/codelabs/debugging-service-workers"],
-							["Caching best practices", "https://jakearchibald.com/2016/caching-best-practices/"],
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111065", ""],
+							["학점 : 3-0-6", ""],
 						]
 					}
 				},
 				{
 					name: "캡스톤디자인I",
 					description: {
-						text: "Learn how to tell the browser about your web app and how it should behave when 'installed' by creating a web app manifest.",
+						text: "Capstone DesignⅠ",
 						links: [
-							["Google Devs - Web App Manifest", "https://developers.google.com/web/fundamentals/web-app-manifest/"],
-							["web.dev - Add a web app manifest", "https://web.dev/add-manifest/"],
-							["W3C - Web App Manifest", "https://www.w3.org/TR/appmanifest/"],
-							["MDN - How to make PWAs installable", "https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Installable_PWAs"],
+							["이수 구분 : 전필", ""],
+							["교과목 번호 : 5111090", ""],
+							["학점 : 2-0-4", ""],
 						]
 					}
 				},
 				{
 					name: "기계학습",
 					description: {
-						text: "Learn how to make your web app installable.",
+						text: "Machine Learning",
 						links: [
-							["web.dev - Installable", "https://web.dev/installable"],
-							["Google Devs - App Install Banners", "https://developers.google.com/web/fundamentals/app-install-banners/"],
-							["MDN - Add to Home screen", "https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Add_to_home_screen"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111078", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "임베디드시스템",
 					description: {
-						text: "Learn how to handle inputs from pointing devices such as a mouse, pen/stylus or touch.",
+						text: "Embedded Systems.",
 						links: [
-							["MDN - Pointer Events", "https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events"],
-							["Google Devs - Add Touch to Your Site", "https://developers.google.com/web/fundamentals/design-and-ux/input/touch"],
-							["Google Devs - Pointing the Way Forward", "https://developers.google.com/web/updates/2016/10/pointer-events"],
-							["Google Devs - Touch Action Options", "https://developers.google.com/web/updates/2016/10/touch-action"],
-							["W3C - Pointer Events", "https://www.w3.org/TR/pointerevents/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111031", ""],
+							["학점 : 3-2-2", ""],
 						]
 					}
 				},
 				{
 					name: "영상처리",
 					description: {
-						text: "Learn how to handle inputs from pointing devices such as a mouse, pen/stylus or touch.",
+						text: "Image Processing",
 						links: [
-							["MDN - Pointer Events", "https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events"],
-							["Google Devs - Add Touch to Your Site", "https://developers.google.com/web/fundamentals/design-and-ux/input/touch"],
-							["Google Devs - Pointing the Way Forward", "https://developers.google.com/web/updates/2016/10/pointer-events"],
-							["Google Devs - Touch Action Options", "https://developers.google.com/web/updates/2016/10/touch-action"],
-							["W3C - Pointer Events", "https://www.w3.org/TR/pointerevents/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111032", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "창업산학초청세미나I",
 					description: {
-						text: "Learn how to handle inputs from pointing devices such as a mouse, pen/stylus or touch.",
+						text: "Entrepreneurship and Educational-Industrial Special SeminarⅠ",
 						links: [
-							["MDN - Pointer Events", "https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events"],
-							["Google Devs - Add Touch to Your Site", "https://developers.google.com/web/fundamentals/design-and-ux/input/touch"],
-							["Google Devs - Pointing the Way Forward", "https://developers.google.com/web/updates/2016/10/pointer-events"],
-							["Google Devs - Touch Action Options", "https://developers.google.com/web/updates/2016/10/touch-action"],
-							["W3C - Pointer Events", "https://www.w3.org/TR/pointerevents/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5100012", ""],
+							["학점 : 1-0-2", ""],
 						]
 					}
 				},
 				{
 					name: "정보,컴퓨터교재연구및지도법",
 					description: {
-						text: "Learn how to handle inputs from pointing devices such as a mouse, pen/stylus or touch.",
+						text: "Computer Instructional Resources and Methods",
 						links: [
-							["MDN - Pointer Events", "https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events"],
-							["Google Devs - Add Touch to Your Site", "https://developers.google.com/web/fundamentals/design-and-ux/input/touch"],
-							["Google Devs - Pointing the Way Forward", "https://developers.google.com/web/updates/2016/10/pointer-events"],
-							["Google Devs - Touch Action Options", "https://developers.google.com/web/updates/2016/10/touch-action"],
-							["W3C - Pointer Events", "https://www.w3.org/TR/pointerevents/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111093", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "빅데이터시스템설계",
 					description: {
-						text: "Learn how to handle inputs from pointing devices such as a mouse, pen/stylus or touch.",
+						text: "Big Data System Design",
 						links: [
-							["MDN - Pointer Events", "https://developer.mozilla.org/en-US/docs/Web/API/Pointer_events"],
-							["Google Devs - Add Touch to Your Site", "https://developers.google.com/web/fundamentals/design-and-ux/input/touch"],
-							["Google Devs - Pointing the Way Forward", "https://developers.google.com/web/updates/2016/10/pointer-events"],
-							["Google Devs - Touch Action Options", "https://developers.google.com/web/updates/2016/10/touch-action"],
-							["W3C - Pointer Events", "https://www.w3.org/TR/pointerevents/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111073", ""],
+							["학점 : 3-2-2", ""],
 						]
 					}
 				}
@@ -143,121 +122,121 @@ export const pwaCollection = {
 				{
 					name: "인턴쉽II",
 					description: {
-						text: "Learn how to use an app shell to provide an instant and reliable experience to users on repeat visits",
+						text: "InternshipⅡ",
 						links: [
-							["Google Devs - The App Shell Model", "https://developers.google.com/web/fundamentals/architecture/app-shell"],
-							["MDN - Architecture of an app", "https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/App_structure"]
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111037", ""],
+							["학점 : 15-0-16주", ""],
 						]
 					}
 				},
 				{
 					name: "인턴쉽III",
 					description: {
-						text: "Learn about render-blocking resources and why they are bad for the user-experience.",
+						text: "InternshipⅢ",
 						links: [
-							["MDN - Render-blocking resources", "https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Loading#Render-blocking_resources"],
-							["Google Devs - Render-Blocking Resources", "https://developers.google.com/web/tools/lighthouse/audits/blocking-resources"]
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111038", ""],
+							["학점 : 3-0-4주", ""],
 						]
 					}
 				},
 				{
 					name: "실무프로젝트II",
 					description: {
-						text: "Learn how to minimize the overall download size by optimizing and compressing resources.",
+						text: "Practical Software ProjectⅡ",
 						links: [
-							["Google Devs - Optimizing Encoding and Transfer Size", "https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/optimize-encoding-and-transfer"],
-							["Google Devs - Replace Animated GIFs with Video", "https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/replace-animated-gifs-with-video/"],
-							["Squoosh", "https://squoosh.app/"],
-							["Google Devs - Text Content", "https://developers.google.com/web/fundamentals/performance/get-started/textcontent-3"],
-							["Google Devs - Optimize Images", "https://developers.google.com/web/tools/lighthouse/audits/optimize-images"],
-							["web.dev - Reduce JavaScript execution time", "https://web.dev/bootup-time/"]
+							["이수 구분 : 일선", ""],
+							["교과목 번호 : 5111066", ""],
+							["학점 : 6-0-12", ""],
 						]
 					}
 				},
 				{
 					name: "창업파일럿프로젝트",
 					description: {
-						text: "Learn why it is important to measure performance and which metrics you should prioritize.",
+						text: "Entrepreneurship Capstone Design",
 						links: [
-							["Google Devs - User-centric Performance Metrics", "https://developers.google.com/web/fundamentals/performance/user-centric-performance-metrics"],
-							["Udacity - Website Performance Optimization", "https://www.udacity.com/course/website-performance-optimization--ud884"],
-							["Udacity - Browser Rendering Optimization", "https://www.udacity.com/course/browser-rendering-optimization--ud860"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111091", ""],
+							["학점 : 2-0-4", ""],
 						]
 					}
 				},
 				{
 					name: "정보검색",
 					description: {
-						text: "Learn how to use lazy loading to lower the initial page payload and load time.",
+						text: "Information Retrieval",
 						links: [
-							["Google Devs - Lazy Loading Images and Video", "https://developers.google.com/web/fundamentals/performance/lazy-loading-guidance/images-and-video"],
-							["Addy Osmani - Lazy Loading Images", "https://addyosmani.com/blog/lazy-loading/"],
-							["web.dev - Native lazy-loading for the web", "https://web.dev/native-lazy-loading/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111042", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "창업산학초청세미나II",
 					description: {
-						text: "Learn about the critical request chains and how to find them in your web app.",
+						text: "Entrepreneurship and Educational-Industrial Special SeminarⅡ",
 						links: [
-							["Google Devs - Critical Request Chains", "https://developers.google.com/web/tools/lighthouse/audits/critical-request-chains"],
-							["web.dev - Minimize critical requests depth", "https://web.dev/critical-request-chains"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5100013", ""],
+							["학점 : 1-0-2", ""],
 						]
 					}
 				},
 				{
 					name: "정보,컴퓨터논리및논술",
 					description: {
-						text: "Learn how to use tree shaking to eliminate dead code.",
+						text: "Logic and Essay Writing in Computer Education",
 						links: [
-							["Google Devs - Reduce JavaScript Payloads", "https://developers.google.com/web/fundamentals/performance/optimizing-javascript/tree-shaking"],
-							["How to Clean Up Your JavaScript", "https://www.keycdn.com/blog/tree-shaking"],
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111095", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "정보,컴퓨터교육론",
 					description: {
-						text: "Learn about codesplitting an how dividing your code into multiple chunks makes your web app faster.",
+						text: "Computer Subject Teaching",
 						links: [
-							["Google Devs - Reduce JavaScript Payloads", "https://developers.google.com/web/fundamentals/performance/optimizing-javascript/code-splitting"],
-							["All you need to know about JavaScript code splitting", "https://www.creativebloq.com/how-to/all-you-need-to-know-about-javascript-code-splitting"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111094", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "자연언어처리",
 					description: {
-						text: "Learn how to use the PRPL pattern to make web apps more performant.",
+						text: "Natural Language Processing",
 						links: [
-							["Google Devs - The PRPL Pattern", "https://developers.google.com/web/fundamentals/performance/prpl-pattern/"],
-							["Google Devs - JavaScript Start-up Optimization", "https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/javascript-startup-optimization"],
-							["Ben Schwarz - Real world performance", "https://building.calibreapp.com/beyond-the-bubble-real-world-performance-9c991dcd5342?gi=76804aa8f46e"],
-							["The Cost of Javascript", "https://medium.com/@addyosmani/the-cost-of-javascript-in-2018-7d8950fbb5d4"],
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111079", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "빅데이터분석시각화",
 					description: {
-						text: "Learn about resource prioritization and how to inform the browser about the importance of a resource.",
+						text: "Big Data Analysis and Visualization",
 						links: [
-							["Google Devs - Resource Prioritization", "https://developers.google.com/web/fundamentals/performance/resource-prioritization"],
-							["Eliminating Roundtrips with Preconnect", "https://www.igvita.com/2015/08/17/eliminating-roundtrips-with-preconnect/"],
-							["Addy Osmani - Preload, Prefetch And Priorities in Chrome", "https://medium.com/reloading/preload-prefetch-and-priorities-in-chrome-776165961bbf"],
-							["web.dev - Preconnect to required origins", "https://web.dev/uses-rel-preconnect/"],
-							["Preload, prefetch and other <link> tags", "https://3perf.com/blog/link-rels/"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111092", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				},
 				{
 					name: "클라우드컴퓨팅",
 					description: {
-						text: "Learn about caching and how it can be uses to make web apps load faster.",
+						text: "Cloud Computing",
 						links: [
-							["Google Devs - HTTP Caching", "https://developers.google.com/web/fundamentals/performance/get-started/httpcaching-6"],
-							["MDN - HTTP caching", "https://developer.mozilla.org/en-US/docs/Web/HTTP/Caching"]
+							["이수 구분 : 전선", ""],
+							["교과목 번호 : 5111072", ""],
+							["학점 : 3-3-0", ""],
 						]
 					}
 				}
